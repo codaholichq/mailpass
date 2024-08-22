813b9720cefaee1b9ce5f61c3c9737f19625c537
Emmy Steven <hello@emmysteven.com>
2024-08-22 12:11:45 +0100

    move files to their appropriate directory

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,16 +1,52 @@
-use tokio::net::TcpListener;
-
 mod routes;
 mod services;
 
+use anyhow::{Context, Result};
 use routes::setup_routes;
+use serde::Deserialize;
+use std::{net::IpAddr, time::Duration};
+use tokio::{
+    net::TcpListener,
+    signal::unix::{signal, SignalKind},
+    time::sleep,
+};
 
-pub async fn run_server() {
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct Config {
+    addr: IpAddr,
+    port: u16,
+    #[serde(with = "humantime_serde")]
+    shutdown_timeout: Option<Duration>,
+}
+
+pub async fn run_server(config: Config) -> Result<()> {
+    let Config {
+        addr,
+        port,
+        shutdown_timeout,
+    } = config;
+
     let app = setup_routes();
 
-    let listener: TcpListener = TcpListener::bind(&"0.0.0.0:3000").await.unwrap();
+    let listener = TcpListener::bind((addr, port))
+        .await
+        .context("bind TcpListener")?;
 
-    println!("listening on {}", listener.local_addr().unwrap());
+    // println!("listening on {}", listener.local_addr().unwrap());
 
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal(shutdown_timeout))
+        .await
+        .context("run server")
+}
+
+async fn shutdown_signal(shutdown_timeout: Option<Duration>) {
+    signal(SignalKind::terminate())
+        .expect("install SIGTERM handler")
+        .recv()
+        .await;
+    if let Some(shutdown_timeout) = shutdown_timeout {
+        sleep(shutdown_timeout).await;
+    }
 }
--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,8 +1,13 @@
 use axum::{
+    body::Body,
+    http::{Request, StatusCode},
     routing::{get, post},
     Json, Router,
 };
 use serde_json::json;
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
+use tracing::{field, info_span, Span};
 
 use crate::services::email_validator::validate_email;
 
@@ -10,6 +15,8 @@ pub fn setup_routes() -> Router {
     Router::new()
         .route("/", get(home))
         .route("/email", post(validate_email))
+        .route("/health", get(health))
+        .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http().make_span_with(make_span)))
 }
 
 async fn home() -> Json<serde_json::Value> {
@@ -17,4 +24,14 @@ async fn home() -> Json<serde_json::Value> {
         "message": "Welcome to the MailPass API V1"
     });
     Json(response)
+}
+
+async fn health() -> StatusCode {
+    StatusCode::NO_CONTENT
+}
+
+fn make_span(request: &Request<Body>) -> Span {
+    let headers = request.headers();
+    let path = request.uri().path();
+    info_span!("incoming request", path, ?headers, trace_id = field::Empty)
 }
