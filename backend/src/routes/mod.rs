79151bbb07e74d264854fc65c5507cbecf1f2e51
Emmy Steven <hello@emmysteven.com>
2024-08-24 13:45:45 +0100

    fix: service does not log errors to the console
    fix #2

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,22 +1,31 @@
 use axum::{
-    body::Body,
-    http::{Request, StatusCode},
+    http::StatusCode,
     routing::{get, post},
     Json, Router,
 };
 use serde_json::json;
-use tower::ServiceBuilder;
-use tower_http::trace::TraceLayer;
-use tracing::{field, info_span, Span};
+use tower_http::cors::{Any, CorsLayer};
+use tower_http::trace::{self, TraceLayer};
+use tracing::Level;
 
 use crate::services::email_validator::validate_email;
 
 pub fn setup_routes() -> Router {
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
+
     Router::new()
         .route("/", get(home))
         .route("/email", post(validate_email))
         .route("/health", get(health))
-        .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http().make_span_with(make_span)))
+        .route("/error", get(error))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+        )
 }
 
 async fn home() -> Json<serde_json::Value> {
@@ -26,12 +35,10 @@ async fn home() -> Json<serde_json::Value> {
     Json(response)
 }
 
-async fn health() -> StatusCode {
-    StatusCode::NO_CONTENT
+async fn health() -> Result<&'static str, StatusCode> {
+    Ok("Service is up and running")
 }
 
-fn make_span(request: &Request<Body>) -> Span {
-    let headers = request.headers();
-    let path = request.uri().path();
-    info_span!("incoming request", path, ?headers, trace_id = field::Empty)
+async fn error() -> Result<&'static str, StatusCode> {
+    Err(StatusCode::SERVICE_UNAVAILABLE)
 }
